a5e479e59452fb481a1c429ed4af8d863cdd4601
Dara <moskalenko.dara@mail.ru>
2022-02-15 15:36:29 +0300

    change2

--- Program.cs
+++ Program.cs
@@ -1,29 +1,25 @@
-﻿Console.Write("Введите положительное целое число: ");
+Random rand = new Random();
+Console.Write("Введите целое число: ");
 
-int value1 = 0;
 try
 {
-    value1 = Convert.ToInt32(Console.ReadLine());
-    
-    if (value1 > 0)
+    var value1 = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"{value1}*2 = {value1 * 2}");
+    Console.WriteLine($"{value1}-20 = {value1 - 20}");
+    if (value1 < 0)
+        value1 = Math.Abs(value1);
+
+    Console.WriteLine($"Массив с размерностью {value1}:");
+    var massive = new int[value1];
+    for (int i = 0; i < massive.Length; i++)
     {
-        
-        Console.WriteLine($"Размерность массива: {value1}");
-    }
-    else
-    {
-        Console.WriteLine("Число не положительное!");
+        massive[i] = rand.Next(-100,100);
+        Console.Write($"{massive[i]} \t");
     }
 }
+
 catch(Exception ex)
 {
     Console.WriteLine(ex.Message);
 }
 
-int[] massive = new int[value1];
-Random rand = new Random();
-for (int i = 0; i < massive.Length; i++)
-{
-    massive[i] = rand.Next(-100,100);
-    Console.Write($"{massive[i]} \t");
-}
